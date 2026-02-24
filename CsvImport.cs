1d42dd428fa6f14511fc0eca24d09ee062ffa758
AyaNS2003 <aya.sawalha.2003@gmail.com>
2026-02-24 15:14:18 +0200

    refactor: restructure project to apply strategy pattern when getting product info

---
+++ CsvImport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagementConsoleApplication
+{
+    internal class CsvImport : IProductInputStrategy
+    {
+        ProductService _productService;
+
+        public CsvImport(ProductService productService) {
+            _productService = productService;
+        }
+        public void Execute()
+        {
+
+        }
+    }
+}
