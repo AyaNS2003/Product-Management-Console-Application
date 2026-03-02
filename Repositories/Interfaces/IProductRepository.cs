d9ca4bcfa1d32b7281db1b3dbcfc5f7eed888467
AyaNS2003 <aya.sawalha.2003@gmail.com>
2026-03-02 20:50:17 +0200

    create repository layer for product management

---
+++ Repositories/Interfaces/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using ProductManagementConsoleApplication.Models;
+
+namespace ProductManagementConsoleApplication.Repositories.Interfaces
+{
+    interface IProductRepository
+    {
+        void Add(Product product);
+        List<Product> GetAll();
+        bool IsCodeUnique(string productCode);
+    }
+}
---
+++ Repositories/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using ProductManagementConsoleApplication.Models;
+using ProductManagementConsoleApplication.Repositories.Interfaces;
+
+namespace ProductManagementConsoleApplication.Repositories
+{
+    class ProductRepository : IProductRepository
+    {
+        private readonly List<Product> _products;
+        public ProductRepository()
+        {
+            _products = new List<Product>();
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+        public bool IsCodeUnique(string productCode)
+        {
+            return !(_products.Any(p => p.ProductCode == productCode));
+        }
+    }
+}
